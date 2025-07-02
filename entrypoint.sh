8fd249f545b52a78d02ca84084bcdba0d54c9380
Nick Driver <driver.nick@gmail.com>
2025-07-02 13:00:49 -0400

    fix permissions from copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,10 @@ cp -Rn /opt/sigma/default/rules/* /opt/sigma/custom/rules/
 
 # 2: Expose a unified view at /sigma
 mkdir -p /sigma
-cp -a /opt/sigma/custom/rules /sigma
-cp -an /opt/sigma/default/rules/* /sigma/
+# cp -a /opt/sigma/custom/rules /sigma
+cp -r --no-preserve=mode,ownership /opt/sigma/custom/rules /sigma
+# cp -an /opt/sigma/default/rules/* /sigma/
+cp -rn --no-preserve=mode,ownership /opt/sigma/default/rules/* /sigma/
 
 # 3: Chainsaw: merge default -> /chainsaw-rules
 #    (host volume at /chainsaw-rules can override/add)
